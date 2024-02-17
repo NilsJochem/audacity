c2da6fabedc9a5a8a94156dff928c513d167e3ae
Nils Jochem <nilsjochem@gmail.com>
2024-02-17 10:55:50 +0100

    added MPL-2.0

--- command-derive/src/lib.rs
+++ command-derive/src/lib.rs
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: 2024 Nils Jochem
+// SPDX-License-Identifier: MPL-2.0
+
 use darling::{FromField, FromVariant};
 use proc_macro2::TokenStream as TokenStream2;
 use quote::quote;
